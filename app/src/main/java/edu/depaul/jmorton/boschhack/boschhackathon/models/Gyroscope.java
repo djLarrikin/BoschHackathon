8f33487b399e1a66481a1b823d1d6de2b09b764b
Ru <fangruivy@gmail.com>
2016-09-21 13:45:40 -0500

    Gyroscope

---
+++ app/src/main/java/edu/depaul/jmorton/boschhack/boschhackathon/models/Gyroscope.java
@@ -0,0 +1,45 @@
+package edu.depaul.jmorton.boschhack.boschhackathon.models;
+
+/**
+ * Created by Ru Fang on 9/21/16.
+ */
+
+public class Gyroscope {
+
+    private float axisX;
+    private float axisY;
+    private float axisZ;
+
+    public Gyroscope() {
+        this.axisX = 0;
+        this.axisY = 0;
+        this.axisZ = 0;
+    }
+
+//    getters and setters
+
+    public void setAxisX(float axisX) {
+        this.axisX = axisX;
+    }
+
+    public void setAxisY(float axisY) {
+        this.axisY = axisY;
+    }
+
+    public void setAixsZ(float aixsZ) {
+        this.axisZ = axisZ;
+    }
+
+    public float getAxisX() {
+        return this.axisX;
+    }
+
+    public float getAxisY() {
+        return this.axisY;
+    }
+
+    public float getAxisZ() {
+        return this.axisZ;
+    }
+}
+
