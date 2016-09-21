9144a8937c5a805d3e428e28e22ffe5d1dc04383
Ru <fangruivy@gmail.com>
2016-09-21 15:36:33 -0500

    Rollover: add dangerous accident alert!

--- app/src/main/java/edu/depaul/jmorton/boschhack/boschhackathon/accidents/Rollover.java
+++ app/src/main/java/edu/depaul/jmorton/boschhack/boschhackathon/accidents/Rollover.java
@@ -4,27 +4,28 @@ package edu.depaul.jmorton.boschhack.boschhackathon.accidents;
  * Created by stefanie on 9/21/16.
  */
 
+import edu.depaul.jmorton.boschhack.boschhackathon.models.CarModel;
 import edu.depaul.jmorton.boschhack.boschhackathon.models.Gyroscope;
+import edu.depaul.jmorton.boschhack.boschhackathon.models.PersonModel;
 
 public class Rollover extends DangerousAccidents {
 
-    //    PersonModel driver;
-//    CarModel car;
+    PersonModel person;
+    CarModel car;
     Gyroscope gyrosocpe;
 
-    public Rollover(Gyroscope gyroscope) {
-//        this.driver = driver;
-//        this.car = car;
+    public Rollover(PersonModel person, CarModel car, Gyroscope gyroscope) {
+        super(person, car);
         checkGyrosocpe(gyroscope);
     }
 
     public void checkGyrosocpe(Gyroscope gyroscope) {
 //      suppose when the car accident happened the rate of rotation is lager than 180 degree per second (pi/2)
         while ((gyroscope.getAxisX() < 1.57) && (gyroscope.getAxisY() < 1.57) && (gyroscope.getAxisZ() < 1.57)) {
-
         }
-
-//        text911();
+        StringBuffer information = new StringBuffer();
+        infromation = getInformation();
+        text911(information);
 
     }
 
